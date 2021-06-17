a07661dd628b8fd4560bf7c728a45bbe378da9ee
Jason Ge <M35620@mcap.com>
2021-06-17 09:15:56 -0400

    Add project files.

---
+++ Notifier.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceProcess;
+
+namespace NotificationService
+{
+    public partial class Notifier : ServiceBase
+    {
+        private readonly System.Timers.Timer timer = null;
+
+        public Notifier()
+        {
+            InitializeComponent();
+
+            timer = new System.Timers.Timer();
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Interval = 10000;
+            timer.Enabled = false;
+
+        }
+
+        /// <summary>
+        /// This class is used for debugging purpose, so we can call it to start the service code during debugging
+        /// </summary>
+        public void StartService()
+        {
+            OnStart(null);
+        }
+
+        /// <summary>
+        /// This class is used for debugging purpose, so we can call it to stop the service code during debugging
+        /// </summary>
+        public void StopService()
+        {
+            OnStop();
+        }
+
+        /// <summary>
+        /// This class is used for debugging purpose, so we can call it to stop the service code during debugging
+        /// </summary>
+        public void ShutdownService()
+        {
+            OnShutdown();
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            int interval = Convert.ToInt32(ConfigurationManager.AppSettings["NotificationInterval"]);
+
+            timer.Enabled = true;
+            timer.Interval = interval;
+            timer.Start();
+            Console.WriteLine("Service started.");
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+            Console.WriteLine("Service stopped.");
+        }
+
+        protected override void OnShutdown()
+        {
+            timer.Stop();
+            timer.Dispose();
+            Console.WriteLine("Service shutdown.");
+        }
+
+        private void SendNotification()
+        {
+            // Do the work here
+            Console.WriteLine("SendNotification called.");
+        }
+
+        private void timer_Elapsed(Object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                //stop timer.
+                timer.Stop();
+
+                SendNotification();
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Start timer again.
+                timer.Start();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NotificationService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            var notifier = new Notifier(); // Create object of your Service's class.
+            while (true)
+            {
+                var key = Convert.ToChar(Console.Read());
+                if (key == 's' || key == 'S')
+                {
+                    notifier.StartService(); // Call the public StartService() method.
+                }
+                else if (key == 't' || key == 'T')
+                {
+                    notifier.StopService(); // Call the public StopService() method.
+                }
+                else if (key == 'e' || key == 'E')
+                {
+                    notifier.ShutdownService(); // Call the public ShutdownService() method.
+                    break;
+                }
+            }
+#else
+            // Normal code to start your service
+            var servicesToRun = new ServiceBase[]
+            {
+                new Notifier()
+            };
+            ServiceBase.Run(servicesToRun);
+#endif
+        }
+    }
+}
